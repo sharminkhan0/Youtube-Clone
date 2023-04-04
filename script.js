51b1e916e60f9905c4bd2559705a4a9f254e88e2
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-04-05 02:44:31 +0600

    modified files

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
 var container = document.querySelector(".container1");
 var containers = document.querySelector(".list-container1");
+var filter = document.querySelector(".filters");
 
 menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container1");
     containers.classList.toggle("large-list-container1");
+    filter.classList.toggle("large-filters");
 }
+
